28181804d9e25d51ea067a95625cfe96f87e5642
wuxue <1203588983@qq.com>
2024-03-26 00:12:05 +0800

    home page init

--- swiftuiLearn/ContentView.swift
+++ swiftuiLearn/ContentView.swift
@@ -9,11 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 0.8) {
+            Spacer()
+            Image("logo")
+                .resizable(resizingMode: /*@START_MENU_TOKEN@*/.stretch/*@END_MENU_TOKEN@*/)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26, height: 26)
+                .cornerRadius(20)
+            Text("SwiftUI for IOS 15")
+                .font(.largeTitle)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            Text("20 sections - 3 hours")
+                .font(.footnote)
+                .fontWeight(.semibold)
+            Text("Build an IOS app for IOS 15 with custom layouts,animations and ...")
+                .font(.footnote)
+                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
         }
-        .padding()
+        .padding(.all, 20.0)
+        .frame(height: 350)
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+        .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+        .shadow(radius: /*@START_MENU_TOKEN@*/20/*@END_MENU_TOKEN@*/)
     }
 }
 
