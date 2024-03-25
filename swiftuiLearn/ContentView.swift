78744d1e8e5969ba9a6a91ce11ca39553568cf6b
wuxue <1203588983@qq.com>
2024-03-25 23:43:05 +0800

    init

---
+++ swiftuiLearn/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  swiftuiLearn
+//
+//  Created by wuxue on 2024/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftuiLearn/swiftuiLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiLearnApp.swift
+//  swiftuiLearn
+//
+//  Created by wuxue on 2024/3/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
