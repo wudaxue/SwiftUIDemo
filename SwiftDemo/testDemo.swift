a38a98511aafd1ac32564630e3d22f495e4fbf1c
wuxue <1203588983@qq.com>
2024-06-11 17:59:23 +0800

    Sequencing

--- SwiftDemo/testDemo.swift
+++ SwiftDemo/testDemo.swift
@@ -8,37 +8,35 @@
 import SwiftUI
 
 struct testDemo: View {
-    @State private var show1 = false
-    @State private var show2 = false
-    @State private var show3 = false
+    @State private var change1 = false
+    @State private var color1 = Color.green
+    @State private var change2 = false
+    @State private var color2 = Color.green
     var body: some View {
-        VStack(spacing: 10) {
-            TitleText("With Animation")
-            SubtitleText("Sequencing")
-            BannerText(_text: "Here is a way to use withAnimation to sequence animations. It's similar to what you learned with earlier with the delay modifier.", backColor: .yellow, textColor: .black)
+        VStack(spacing: 20) {
+            Circle()
+                .fill(color1)
+                .frame(height: 100)
+                .frame(maxWidth: .infinity, alignment: change1 ? .trailing : .leading)
+            Circle()
+                .fill(color2)
+                .frame(height: 100)
+                .frame(maxWidth: .infinity, alignment: change2 ? .trailing : .leading)
             Button("Change") {
-                withAnimation(.default) {
-                    show1.toggle() // Animation 1
+                withAnimation(.spring(bounce: 0.6), completionCriteria: .logicallyComplete) {
+                    change1.toggle()
+                } completion: {
+                    color1 = .red
                 }
-                withAnimation(.default.delay(0.4)) {
-                    show2.toggle() // Animation 2
-                }
-                withAnimation(.default.delay(0.8)) {
-                    show3.toggle() // Animation 3
+                withAnimation(.spring(bounce: 0.6), completionCriteria: .removed) {
+                    change2.toggle()
+                } completion: {
+                    color2 = .red
                 }
             }
-            Spacer()
-            VStack(spacing: 20) {
-                Image(systemName: "1.circle")
-                    .opacity(show1 ? 1 : 0)
-                Image(systemName: "2.circle")
-                    .opacity(show2 ? 1 : 0)
-                Image(systemName: "3.circle")
-                    .opacity(show3 ? 1 : 0)
-            }
-            .font(.system(size: 80))
-            Spacer()
-        }.font(.title)
+            .font(.title)
+            .buttonStyle(.borderedProminent)
+        }
     }
 }
 
